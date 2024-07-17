e5ff2d9965ba0ec28b252015b0fdb3f15d043c50
sahilpan07 <sahilpan993@gmail.com>
2024-07-17 21:44:34 +0545

    Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <div>
       <div className="app">
         <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/cart' element={}
+        </Routes>
       </div>
     </div>
   )
