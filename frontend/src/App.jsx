4f6ec0d35b1a304a27d8ff8522dcd903ebacfd99
sahilpan07 <sahilpan993@gmail.com>
2024-07-18 20:09:20 +0545

    Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
@@ -10,7 +12,8 @@ const App = () => {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
-          <Route path='/cart' element={}
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/placeorder' element={<PlaceOrder/>}/>
         </Routes>
       </div>
     </div>
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your favourite Food Here</h2>
+            <p>Easily Order. Order your favourite foods and essentials through Foodganj. Quick Delivery. Get your orders delivered to your doorstep through our Website</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
