9777ebffa071f443e1d32a7dfab6d7838cb17602
sahilpan07 <sahilpan993@gmail.com>
2024-07-28 18:28:39 +0545

    Commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import App from '../../components/App/App'
 
 const Home = () => {
   const [category,setCategory] = useState("All");
@@ -12,6 +13,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category}/>
+      <App/>
     </div>
   )
 }
