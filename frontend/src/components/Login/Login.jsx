fa38e2b512a25bef05be262d62232aba0548858d
sahilpan07 <sahilpan993@gmail.com>
2024-07-30 18:41:45 +0545

    Commit

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='login-popup'>
+        Login
+    </div>
+  )
+}
+
+export default Login
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
+import {Link} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu,setMenu] = useState("home")
 
   return (
     <div className='navbar'>
         <img src={assets.logo} className='logo' />
         <ul className="navbar-menu">
-          <li onClick={()=>setMenu("home")} className={menu=="home"?"active":""}>Home</li>
-          <li onClick={()=>setMenu("menu")} className={menu=="menu"?"active":""}>Menu</li>
-          <li onClick={()=>setMenu("mobile-app")} className={menu=="mobile-app"?"active":""}>Mobile-app</li>
-          <li onClick={()=>setMenu("contact-us")} className={menu=="contact-us"?"active":""}>Contact us</li>
+          <Link to='/' onClick={()=>setMenu("home")} className={menu=="home"?"active":""}>Home</Link>
+          <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu=="menu"?"active":""}>Menu</a>
+          <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu=="mobile-app"?"active":""}>Mobile-app</a>
+          <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu=="contact-us"?"active":""}>Contact us</a>
         </ul> 
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
@@ -20,7 +21,9 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt="" />
             <div className="dot"></div>
           </div>
-          <button className='bg-red-500 py-4 px-8 rounded text-xl hover:bg-red-300 transition-color duration-200'>Sign in</button>
+          <button onClick={()=>setShowLogin(true)} className='bg-red-500 py-4 px-8 rounded text-xl hover:bg-red-300 transition-color duration-200'>
+            Sign in
+          </button>
         </div>
     </div>
   )
