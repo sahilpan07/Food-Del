988ef9fcb60ec8cf86c0dc3e8f0ff2be2b864097
sahilpan07 <sahilpan993@gmail.com>
2024-08-02 17:12:09 +0545

    Commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} className='logo' />
+        <Link to='/'><img src={assets.logo} className='logo' /></Link>
         <ul className="navbar-menu">
           <Link to='/' onClick={()=>setMenu("home")} className={menu=="home"?"active":""}>Home</Link>
           <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu=="menu"?"active":""}>Menu</a>
@@ -18,7 +18,7 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
           <img src={assets.search_icon} alt="" />
           <div className="navbar-search-icon ">
-            <img src={assets.basket_icon} alt="" />
+            <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
             <div className="dot"></div>
           </div>
           <button onClick={()=>setShowLogin(true)} className='bg-red-500 py-4 px-8 rounded text-xl hover:bg-red-300 transition-color duration-200'>
