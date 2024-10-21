4a5c9d8477db67f7c8411949739928ed1f650d9d
sahilpan07 <sahilpan993@gmail.com>
2024-10-21 11:07:06 +0545

    some chnage in contact

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,38 +1,50 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 
 const contactController = async (req, res) => {
-    const { first_name, last_name, phone, address, subject, message } = req.body;
-    const userEmail = req.body.userEmail; 
+  const {
+    first_name,
+    last_name,
+    phone,
+    contact_email,
+    address,
+    subject,
+    message,
+    email,
+  } = req.body;
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: "test423559@gmail.com", 
-            pass: 'bknaqrqdyiewltem', 
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "test423559@gmail.com",
+      pass: "bknaqrqdyiewltem",
+    },
+  });
 
-    const mailOptions = {
-        from: userEmail, 
-        to: 'test423559@gmail.com', 
-        subject: `New message from ${first_name} ${last_name}: ${subject}`,
-        text: `
+  const mailOptions = {
+    from: email,
+    to: "test423559@gmail.com",
+    subject: `New message from ${first_name} ${last_name}: ${subject}`,
+    text: `
             Name: ${first_name} ${last_name}
-            Email: ${userEmail}
+            Email: ${contact_email}
             Phone: ${phone}
             Address: ${address}
             Message: ${message}
         `,
-    };
+  };
 
-    try {
-        await transporter.sendMail(mailOptions);
-        return res.status(200).json({ success: true, message: 'Email sent successfully' });
-    }  catch (error) {
-        console.error('Error sending email:', error.message);
-        return res.status(500).json({ message: 'Error sending email', error: error.message });
-    }
+  try {
+    await transporter.sendMail(mailOptions);
+    return res
+      .status(200)
+      .json({ success: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error.message);
+    return res
+      .status(500)
+      .json({ message: "Error sending email", error: error.message });
+  }
 };
 
 export default contactController;
