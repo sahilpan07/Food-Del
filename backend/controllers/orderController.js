eb6a670f750f09ae41ef1333b9fc8255891a8622
sahilpan07 <sahilpan993@gmail.com>
2024-12-10 15:19:17 +0545

    ss

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const placeOrder = async (req, res) => {
   const frontend_url = "http://localhost:5173";
   try {
-    const newOrder = new orderModel({
+    const newOrder = new orderModel({ 
       userId: req.body.userId,
       items: req.body.items,
       amount: req.body.amount,
